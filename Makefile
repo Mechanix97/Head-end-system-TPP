3cbb1f38f354f8f2e2aa2df1542ac49d45b37310
Mechardo <lucas_rack@live.com.ar>
2025-08-16 17:38:27 -0300

    Add workspace dependencies

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-.PHONY: build lint
+.PHONY: build run lint
 
 build: ## Build the server
 	cargo build
+
+run: ## run the server
+	cargo run
 
 lint: ## Linter check
 	cargo clippy --all-targets --all-features -- -D warnings
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use tracing::info;
+
+use scheduler::scheduler::init_scheduler;
+
 fn main() {
-    // init_scheduler();
+    tracing_subscriber::fmt().init();
+
+    info!("Head-End System starting");
+
+    init_scheduler();
 
     // init_backdoor();
 }
