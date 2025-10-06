ddebf864c6758249e5ce79b1fb8ee06625dc7d43
Mechardo <lucas_rack@live.com.ar>
2025-10-06 15:25:31 -0300

    Add shutdown

--- src/scheduler/scheduler.rs
+++ src/scheduler/scheduler.rs
@@ -22,26 +22,33 @@ impl Scheduler {
 
     pub async fn start(&mut self) -> Result<(), SchedulerError> {
         self.job_scheduler.start().await?;
+        self.job_scheduler.shutdown_on_ctrl_c();
+        self.job_scheduler.set_shutdown_handler(Box::new(|| {
+            Box::pin(async move {
+                info!("Shuting down job scheduler");
+            })
+        }));
         Ok(())
     }
 
     pub async fn add_connection(&mut self, connection: Connection) -> Result<(), SchedulerError> {
-        self.buckets[0].push(connection.clone());
-
         METRICS_CONNECTIONS
             .connections_tracker
             .with_label_values(&["new_connection"])
             .inc();
 
+        let cc2 = connection.clone();
         self.job_scheduler
             .add(Job::new_async("1/10 * * * * *", move |_uuid, _l| {
-                let cc = connection.clone();
+                let cc = cc2.clone();
 
                 Box::pin(async move {
                     periodically_task(cc.id, cc.ip).await;
                 })
             })?)
             .await?;
+        self.buckets[0].push(connection.clone());
+
         Ok(())
     }
 }
