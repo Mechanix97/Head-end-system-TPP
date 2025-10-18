69f2f55d48732f2e9e57580f085eb78998953d91
Mechardo <lucas_rack@live.com.ar>
2025-10-18 19:26:33 -0300

    fix db url

--- src/common/database/postgres.rs
+++ src/common/database/postgres.rs
@@ -14,7 +14,7 @@ impl PostgresDB {
         Self {
             pool: PgPoolOptions::new()
                 .max_connections(5)
-                .connect("postgres://postgres:password@postgres:5432/hes") // TODO fix this
+                .connect("postgres://postgres:password@127.0.0.1:5432/hes") // TODO fix this
                 .await
                 .expect("Error connecting to DB"), // TODO remove this expect
         }
