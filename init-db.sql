5d204405b1612ad0c453e5d78df91ac9245afcdf
Mechardo <lucas_rack@live.com.ar>
2025-10-20 21:39:52 -0300

    fix ip in db

--- init-db.sql
+++ init-db.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS T_ACTIVE_CONNECTIONS (
     device_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     connection_time TIMESTAMP NOT NULL DEFAULT NOW(),
     next_wakeup TIMESTAMP,
+    ip VARCHAR(20),
     status VARCHAR(20) NOT NULL DEFAULT 'active',
     CONSTRAINT valid_status CHECK (status IN ('active', 'pending', 'closed'))
 );
