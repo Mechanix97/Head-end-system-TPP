87a661a6c4cc7c9f02aa5cc51dd0f772ec7f851e
Mechardo <lucas_rack@live.com.ar>
2026-05-12 20:43:30 -0300

    handshake response status = 0 temp

--- src/common/messages/handshake.rs
+++ src/common/messages/handshake.rs
@@ -21,7 +21,8 @@ impl HandshakeMessage {
 
     pub fn decode(data: &[u8]) -> Result<Self, MsgCodecError> {
         let rlp = rlp::Rlp::new(data);
-        let nonce = rlp.val_at(0)
+        let nonce = rlp
+            .val_at(0)
             .map_err(|e| MsgCodecError::PayloadDecodeError(e.to_string()))?;
         Ok(Self { nonce })
     }
@@ -45,9 +46,10 @@ impl HandshakeResponseMessage {
     }
 
     pub fn decode(data: &[u8]) -> Result<Self, MsgCodecError> {
-        let rlp = rlp::Rlp::new(data);
-        let status = rlp.val_at(0)
-            .map_err(|e| MsgCodecError::PayloadDecodeError(e.to_string()))?;
+        let _rlp = rlp::Rlp::new(data);
+        let status: u8 = 0;
+        // rlp.val_at(0)
+        //     .map_err(|e| MsgCodecError::PayloadDecodeError(e.to_string()))?;
         Ok(Self { status })
     }
 }
